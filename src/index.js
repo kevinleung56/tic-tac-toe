a91071d086cb8d6f38ec939642522dc0995f8db8
kevinleung56 <kevinleung56@gmail.com>
2020-08-06 22:11:34 +1000

    improvement 2

--- src/index.js
+++ src/index.js
@@ -99,7 +99,9 @@ class Game extends React.Component {
                 'Go to game start';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button
+                    className={move === this.state.stepNumber ? 'move-list-item-selected': ''}
+                    onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
         });
